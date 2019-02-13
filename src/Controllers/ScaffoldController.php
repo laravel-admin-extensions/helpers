506b2b8577ae2f1cf7563a125a129c31d9f480e6
marwa mourad <marwa.mourad.15@gmail.com>
2019-02-13 13:59:59 +0200

    f

--- src/Controllers/ScaffoldController.php
+++ src/Controllers/ScaffoldController.php
@@ -55,7 +55,7 @@ class ScaffoldController extends Controller
             // 2. Create controller.
             if (in_array('controller', $request->get('create'))) {
                 $paths['controller'] = (new ControllerCreator($request->get('controller_name')))
-                    ->create($request->get('model_name'));
+                    ->create($request->get('model_name') , $request->get('fields'));
             }
 
             // 3. Create migration.
